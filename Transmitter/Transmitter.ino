6bc357f030dfc4b6001cd473b5382765e3c7e2f1
Zhizhou He <eric0813@vt.edu>
2024-03-17 16:49:45 -0400

    Update Transmitter.ino

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -2,14 +2,18 @@
 #ifdef __AVR__
  #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
-#define PIN        6
-#define NUMPIXELS 1
-Adafruit_NeoPixel pixels(14, PIN, NEO_GRB + NEO_KHZ800);
-#define DELAYVAL 500
 
-char result[500]={'1','0','1','0','1','0','1','1','1','1','1','1','1','1','1','1'};
-const char original[500] = {'1','0','1','0','1','0','1','1','1','1','1','1','1','1','1','1'};
-int counter=16;
+#define PIN        6
+#define NUMPIXELS 14
+Adafruit_NeoPixel pixels(14, PIN, NEO_GRB + NEO_KHZ800);
+#define DELAYTIME 5
+#define AUDIOPIN A2
+#define TEXT 0
+#define CYCLE 5
+
+char result[500] = {'1','0','1','0','0','1','0','0','1','1','1'};
+const char original[500] = {'1','0','1','0','0','1','0','0','1','1','1'};
+int counter=11;
 uint32_t colorcol[4] = {pixels.Color(0,   0,   0,255),pixels.Color(255,   0,   0),pixels.Color(0,   255,   0),pixels.Color(0,   0,   255)};
 
 void setup() {
@@ -20,6 +24,8 @@ void setup() {
   pixels.begin();
   pixels.setBrightness(100);
   pixels.setAllWhite();
+  Serial.begin(9600);
+  pinMode(AUDIOPIN, INPUT);
 }
 
 void chartobin(char c)
@@ -49,6 +55,28 @@ uint32_t colortype(int colorindex){
   
 }
 
+
+
+void audiotobin() {
+  int RUN = 0;  
+  while (RUN < CYCLE) {
+    delay(DELAYTIME/CYCLE);
+    int aud = analogRead(AUDIOPIN);
+    Serial.println(aud);
+    for (int i = 0; i < 10; i++) {
+      result[counter] = (aud & (int)1 << (10-i-1)) ? '1' : '0';
+      counter++;
+    }
+    RUN++;
+  }
+  
+  result[(11 + CYCLE * 10)] = '\0';
+  // char buffer[25];
+  // sprintf(buffer,"%s", result[11]);
+  // Serial.println(buffer);
+  // Serial.println("%s", result[11]);
+}
+
 void colorset(int colorindex){
   // for(int i=0; i<14;i++){
   //   pixels.setPixelColor(i,colorcol[colorindex]);
@@ -76,16 +104,20 @@ void signoff(){
 void loop() {
   // put your main code here, to run repeatedly:
   int pos = 0;
+  
+#if TEXT
   char* msg = {"ABCDEFG"};
-  int2bin(strlen(msg)*8, 16);
-
+  int2bin(strlen(msg)*8, 10);
   for (int k = 0; k < strlen(msg); k++) {
     chartobin(msg[k]);
   }
+#else 
+  audiotobin();
+#endif
 
   int length = strlen(result);
   while (pos != length) {
-    delay(50);
+    delay(DELAYTIME);
     if (result[pos] == '1') {
       signon(0);
     }
@@ -94,8 +126,8 @@ void loop() {
     }
     pos++;
   }
-  counter = 16;
-  memcpy(result, original, sizeof(char)*16);
+  counter = 11;
+  memcpy(result, original, sizeof(char)*11);
   // sign0();
   // delay(10);
   pixels.clear();
