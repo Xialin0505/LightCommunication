62e93f81a73be35ad0831135e9c397d380ec9e61
IanFan1999 <yiqunfan@andrew.cmu.edu>
2024-04-14 16:14:07 -0400

    Update transmitter
    Update transmitter to enable 2 method

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -3,47 +3,92 @@
 #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
-#define NOTE_C4 262
-#define NOTE_D4 294
-#define NOTE_E4 330
-#define NOTE_F4 349
-#define NOTE_G4 392
-#define NOTE_A4 440
-#define NOTE_B4 494
+// 1 is on off key
+// 2 is color
+#define MODULATION 1
+
+#define WS 0
+#define CC 262 
+#define DD 294 
+#define EE 330 
+#define FF 349 
+#define GG 392 
+#define AA 440 
+#define BB 494 
 #define END -1
 
-int melody[] = {
-    NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, END};
+// int melody[] = {
+//     NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C3, END};
+  
+int newmelody[] = { CC, CC, GG,GG,AA,AA,GG, WS ,FF, FF, EE, EE, DD, DD, CC, WS, GG, GG, FF, FF, EE, EE, DD, WS, GG, GG, FF, FF, EE, EE, DD, WS, CC, CC, GG, GG, AA, AA, GG, WS, FF, FF, EE, EE, DD, DD, CC, END};
+
+int newnoteDurations[] = {
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
 
 int noteDurations[] = {
     4, 4, 4, 4, 4, 4, 4, 4};
 
 #define PIN 6
+#if (MODULATION == 2)
+#define NUMPIXELS 1
+#define DELAYTIME 200
+#else
 #define NUMPIXELS 14
-Adafruit_NeoPixel pixels(14, PIN, NEO_GRB + NEO_KHZ800);
-#define DELAYTIME 100
+#define DELAYTIME 50
+#endif
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
 #define AUDIOPIN A2
 #define TEXT 0
 #define CYCLE 50
 
-char result[500] = {'1', '0', '1', '0', '0', '1', '0', '0', '1', '1', '1'};
-const char original[500] = {'1', '0', '1', '0', '0', '1', '0', '0', '1', '1', '1'};
-int counter = 11;
+char result[500] = {'1', '0', '1', '0', '0', '1', '0', '0', '1', '1', '1','1'};
+const char original[500] = {'1', '0', '1', '0', '0', '1', '0', '0', '1', '1', '1','1'};
+int counter = 12;
 uint32_t colorcol[4] = {pixels.Color(0, 0, 0, 255), pixels.Color(255, 0, 0), pixels.Color(0, 255, 0), pixels.Color(0, 0, 255)};
 int thisNote = 0;
 
+
 void setup()
 {
-    // put your setup code here, to run once:
+    if (MODULATION == 1) {
+        setupOnOff();
+    } else {
+        setupColor();
+    }
+}
+
+void setupOnOff() {
+        // put your setup code here, to run once:
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
     clock_prescale_set(clock_div_1);
 #endif
     pixels.begin();
     pixels.setBrightness(100);
+    pixels.clear();
     pixels.setAllWhite();
     Serial.begin(9600);
     pinMode(AUDIOPIN, INPUT);
 }
+
+void setupColor() {
+        // put your setup code here, to run once:
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+    clock_prescale_set(clock_div_1);
+#endif
+    pixels.begin();
+    pixels.setBrightness(100);
+    pixels.clear();
+    pixels.setAllBlue();
+    pixels.setAllGreen();
+    pixels.setAllYellow();
+    pixels.setAllPurple();
+    Serial.begin(9600);
+    pinMode(AUDIOPIN, INPUT);
+}
+
+
 
 void chartobin(char c)
 {
@@ -95,39 +140,42 @@ uint32_t colortype(int colorindex)
 
 void melodytobin(int i)
 {
-    int note = melody[i];
-    int noteDuration = noteDurations[i];
-    if (note == NOTE_C4)
+    int note = newmelody[i];
+    int noteDuration = newnoteDurations[i];
+    if (note == CC)
     { // NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, END
         note = 0;
     }
-    else if (note == NOTE_D4)
+    else if (note == DD)
     {
         note = 1;
     }
-    else if (note == NOTE_E4)
+    else if (note == EE)
     {
         note = 2;
     }
-    else if (note == NOTE_F4)
+    else if (note == FF)
     {
         note = 3;
     }
-    else if (note == NOTE_G4)
+    else if (note == GG)
     {
         note = 4;
     }
-    else if (note == NOTE_A4)
+    else if (note == AA)
     {
         note = 5;
     }
-    else if (note == NOTE_B4)
+    else if (note == BB)
     {
         note = 6;
     }
+    else if (note == WS){
+        note = 7;
+    }
     else if (note == END)
     {
-        note = 7;
+        note = 8;
     }
     Serial.println((String) "Note: " + note + "; Dur: " + noteDuration);
 
@@ -142,7 +190,7 @@ void melodytobin(int i)
         counter++;
     }
 
-    result[11 + 6] = '\0';
+    result[12 + 6] = '\0';
 }
 
 void colorset(int colorindex)
@@ -159,7 +207,17 @@ void signon(int colornum)
     // for(int i=0; i<14; i++) { // For each pixel...
     //   pixels.setPixelColor(i,pixels.Color(255, 255, 255));
     // }
-    pixels.resetColor();
+    if(colornum ==0){
+        pixels.resetColor(0);
+    }else if(colornum ==1){
+        pixels.resetColor(1);
+    }else if(colornum ==2 ){
+        pixels.resetColor(2);
+    }else if(colornum ==3){
+        pixels.resetColor(3);
+    }else if(colornum ==4){
+        pixels.resetColor(4);
+    }
     pixels.show();
     return;
 }
@@ -171,9 +229,7 @@ void signoff()
     return;
 }
 
-void loop()
-{
-    // put your main code here, to run repeatedly:
+void transmissionColor() {
     int pos = 0;
 
 #if TEXT
@@ -185,7 +241,74 @@ void loop()
     }
 #else
     // audiotobin();
-    if (melody[thisNote] == -1)
+    if (newmelody[thisNote] == -1)
+    {
+        thisNote = 0;
+    }
+    melodytobin(thisNote);
+    thisNote++;
+    Serial.println(result);
+
+#endif
+//00 blue, 01 yellow, 10 purplr, 11 green
+
+
+    int length = strlen(result);
+    while (pos != length)
+    {
+        // signon(4);
+        delay(DELAYTIME);
+        Serial.print("\n");
+        if (result[pos] == '0' && result[pos+1] =='0')
+        {
+          Serial.print("00");
+          signon(3);
+        }
+        if (result[pos] == '0' && result[pos+1] =='1')
+        {
+          Serial.print("01");
+          signon(1);
+        }
+        if (result[pos] == '1' && result[pos+1] =='0')
+        {
+          Serial.print("10");
+          signon(4);
+        }
+        else if (result[pos] == '1' && result[pos+1] =='1')
+        {
+          Serial.print("11");
+          signon(2);
+          // signoff();
+        }
+        pos+=2;
+        // delay(DELAYTIME/2);
+        // pixels.clear();
+        // pixels.show();
+    }
+    // Serial.print("\n");
+    counter = 11;
+    memcpy(result, original, sizeof(char) * 11);
+    // sign0();
+    // delay(10);
+    pixels.clear();
+    pixels.show();
+    // delay(1000);
+}
+
+void transmissionOnOff() {
+      // put your main code here, to run repeatedly:
+    int pos = 0;
+
+#if TEXT
+    char *msg = {"ABCDEFG"};
+    int2bin(strlen(msg) * 8, 10);
+    for (int k = 0; k < strlen(msg); k++)
+    {
+        chartobin(msg[k]);
+    }
+#else
+    // audiotobin();
+    if (newmelody[thisNote] == -1)
     {
         thisNote = 0;
     }
@@ -198,6 +321,7 @@ void loop()
     int length = strlen(result);
     while (pos != length)
     {
+        // signon(4);
         delay(DELAYTIME);
         if (result[pos] == '1')
         {
@@ -207,6 +331,7 @@ void loop()
         else if (result[pos] == '0')
         {
           // Serial.print("0");
+        //   signon(1);
           signoff();
         }
         pos++;
@@ -219,4 +344,13 @@ void loop()
     pixels.clear();
     pixels.show();
     // delay(1000);
+}
+
+void loop()
+{
+    if (MODULATION == 1) {
+        transmissionOnOff();
+    } else {
+        transmissionColor();
+    }
 }
